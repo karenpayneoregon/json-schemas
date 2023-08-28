45b3ab2a2d64008e5c670efbe935c5e9c8a0ec20
Karen Payne <paynekaren@comcast.net>
2023-08-28 06:57:05 -0700

    Add project files.

---
+++ ConfigurationMapAsp.cs
@@ -0,0 +1,23 @@
+﻿
+namespace WinFormsApp1
+{
+    public enum ConfigurationMapAsp
+    {
+        Development,
+        Stage,
+        Production
+    }
+
+    public class Logging
+    {
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public class LogLevel
+    {
+        public string Default { get; set; }
+        public string MicrosoftAspNetCore { get; set; }
+    }
+
+
+}
